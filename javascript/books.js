e07a0e5c921038b964620204a7ae79b473176a57
Nemehjaaaserzit <>
2021-05-29 20:29:12 +0000

    new style with javas

---
+++ javascript/books.js
@@ -0,0 +1,20 @@
+
+var userName = prompt("Whats your name ?");
+console.log(userName);
+alert("welcome to our websit " + userName );
+   document.write("<p> Welcome to the circle </p>");
+ document.write("<footer> 2021 bt nemeh abuzitoun </footer>");
+
+var age = prompt("Whats your age ?");
+
+if (age > 18) {
+ alert(" enjoy our books ,Good day ");
+}
+else
+{
+  alert(" try  our kids books websit ");
+}
+
+
+
+
