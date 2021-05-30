d29241510a9bc9cf47c2d0dec3359b6de3b83cb2
Nemehjaaaserzit <>
2021-05-30 17:21:06 +0000

    new addetion

--- javascript/books.js
+++ javascript/books.js
@@ -15,6 +15,16 @@ else
   alert(" try  our kids books websit ");
 }
 
+var books = ["scince", "licture", "historical"];
+var i = 0;
+var text = "";
 
-
+for (;books[i];) {
+  text += books[i] + "<br>";
+  i++;
+}
+for (i = 0; i < books.length; i++) {
+  <img src="https://media.npr.org/assets/img/2020/09/18/gettyimages-1170941183-c1e33a9a2b274d5381f14d18f71db5ee0342aec3-s300-c85.webp" width="250px" alt="AOT"/>
+  text += books[i] + "<br>";
+}
 
