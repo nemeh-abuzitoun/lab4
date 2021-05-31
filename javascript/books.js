e8209120a5497c27cfca33587d9b11b4f0e217e3
nemeh abuzitoun <84705453+nemeh-abuzitoun@users.noreply.github.com>
2021-05-31 16:23:41 +0300

    Update books.js

--- javascript/books.js
+++ javascript/books.js
@@ -1,19 +1,21 @@
 
-// var userName = prompt("Whats your name ?");
-// console.log(userName);
-// alert("welcome to our websit " + userName );
-//    document.write("<p> Welcome to the circle </p>");
-//  document.write("<footer> 2021 by nemeh abuzitoun </footer>");
+var userName = prompt("Whats your name ?");
+console.log(userName);
+alert("welcome to our websit " + userName );
+   document.write("<p> Welcome to the circle </p>");
+ document.write("<footer> 2021 by nemeh abuzitoun </footer>");
 
-// var age = prompt("Whats your age ?");
+var age = prompt("Whats your age ?");
 
-// if (age > 18) {
-//  alert(" enjoy our books ,Good day ");
-// }
-// else
-// {
-//   alert(" try  our kids books websit ");
-// }
+if (age > 18) {
+ alert(" enjoy our books ,Good day ");
+}
+else
+{
+  alert(" try  our kids books websit ");
+}
+
+
 alert("hello dear customer");
 var books  = prompt("do you know what kindes of books we offer in our library ?");
 while (books!="historical" && books!="science"){
@@ -30,8 +32,4 @@ var bookCover = prompt("how many books do you want see");
 
 for( var j =0 ; j<bookCover; j++){
   document.write('<img src="https://images-platform.99static.com//pSowFbcSptftAVGPO3Le0PCQhQE=/fit-in/590x590/99designs-contests-attachments/62/62255/attachment_62255190"/>' );
-
-
 }
-
-
