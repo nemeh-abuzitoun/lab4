a886d15f2e916c9ba86bee01ed8b9fc955723f20
Nemehjaaaserzit <>
2021-05-31 12:04:05 +0000

    new addition

--- javascript/books.js
+++ javascript/books.js
@@ -1,57 +1,60 @@
 
-var userName = prompt("Whats your name ?");
-console.log(userName);
-alert("welcome to our websit " + userName );
-   document.write("<p> Welcome to the circle </p>");
- document.write("<footer> 2021 by nemeh abuzitoun </footer>");
+// var userName = prompt("Whats your name ?");
+// console.log(userName);
+// alert("welcome to our websit " + userName );
+//    document.write("<p> Welcome to the circle </p>");
+//  document.write("<footer> 2021 by nemeh abuzitoun </footer>");
 
-var age = prompt("Whats your age ?");
+// var age = prompt("Whats your age ?");
 
-if (age > 18) {
- alert(" enjoy our books ,Good day ");
-}
-else
-{
-  alert(" try  our kids books websit ");
-}
+// if (age > 18) {
+//  alert(" enjoy our books ,Good day ");
+// }
+// else
+// {
+//   alert(" try  our kids books websit ");
+// }
 
-var books  = prompt("Whats your fav books ?");
-var books  = ["a", "b", "c", "d", "e", "f"];
-var text = "";
-var i;
-var type ;
-var type = prompt("Whats your fav book types ?");
-var books  = prompt("Whats your fav books ?");
-for (i = 0; i < books.type; i++) {
+// var books  = prompt("Whats your fav books ?");
+// var books  = ["a", "b", "c", "d", "e", "f"];
+// var text = "";
+// var i;
+// var type ;
+// var type = prompt("Whats your fav book types ?");
+// var books  = prompt("Whats your fav books ?");
+// for (i = 0; i < books.type; i++) {
 
-document.write('<img scr-"https://media.istockphoto.com/photos/books-picture-id949118068?s=612x612"/>');
+// document.write('<img scr-"https://media.istockphoto.com/photos/books-picture-id949118068?s=612x612"/>');
 
-    document.write("<p> type your fav book  </p>");
+//     document.write("<p> type your fav book  </p>");
 
-  text += books[i] + "<br>";
+//   text += books[i] + "<br>";
   
-}
-var text = "";
-var i = 0;
-while (i <4) {
+// }
+// var text = "";
+// var i = 0;
+// while (i <4) {
   
-  text += "<br>The book is " + i;
-  i++;
-  var books = prompt("type your fav book ?");
-}
+//   text += "<br>The book is " + i;
+//   i++;
+//   var books = prompt("type your fav book ?");
+// }
 
 
- let bookTitle = prompt('Please enter your book Title');
+ let bookTitle = prompt('Please enter your bookTitle');
+
+
  let writerName = prompt('Please enter the writerName');
 
 
  function bookInfo(bookTitle, writerName) {
    console.log('bookInfo', bookTitle, writerName);
-  document.write(`Fullname is <b> ${bookTitle} </b> ${writerName} <br>`);
+  document.write(`bookInfo<b> ${bookTitle} </b> ${writerName} <br>`);
  }
 
-if (bookTitle=="") {
-bookTitle = prompt('Please enter your book Title');
+if ( bookTitle === "") {
+  print bookInfo(bookTitle, writerName) 
+
 }
 
 
