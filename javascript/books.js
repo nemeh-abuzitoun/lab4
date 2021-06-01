7c8e76a4b855a9f9611012cec28bc97a5e365e8f
nemeh abuzitoun <84705453+nemeh-abuzitoun@users.noreply.github.com>
2021-06-01 16:01:22 +0300

    Update books.js

--- javascript/books.js
+++ javascript/books.js
@@ -33,3 +33,34 @@ var bookCover = prompt("how many books do you want see");
 for( var j =0 ; j<bookCover; j++){
   document.write('<img src="https://images-platform.99static.com//pSowFbcSptftAVGPO3Le0PCQhQE=/fit-in/590x590/99designs-contests-attachments/62/62255/attachment_62255190"/>' );
 }
+
+function bookIndex(a) {
+  
+  
+  
+  while (books!="historical" && books!="science"){
+
+    alert(" hint. try choose one of these kindes : historical and science");
+    
+    }
+
+  var books  = prompt("do you know what kindes of books we offer in our library ?");
+ 
+
+
+
+
+
+
+  return a  
+}
+
+function bookIndex(a) {
+  
+for( var j =0 ; j<bookCover; j++){
+
+  document.write('<img src="https://images-platform.99static.com//pSowFbcSptftAVGPO3Le0PCQhQE=/fit-in/590x590/99designs-contests-attachments/62/62255/attachment_62255190"/>' );
+  
+}
+  return a  
+}
